ff3abf7dff90ef11393364f3a8cdc78f7819dcc2
Georgij <7364400@gmail.com>
2022-10-11 13:19:43 +0300

    In develop

---
+++ online_shop_tms/shop_app/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Discount(models.Model):
+    name = models.CharField(max_length=50)
+    percent = models.IntegerField()
+    allow_to_sum_with_promo = models.BooleanField()
+
+
+class ProductItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    count_on_stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='media/products', null=True, blank=True, default=None)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
---
+++ online_shop_tms/shop_app/views.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Category, Discount, ProductItem
+from .serializers import CategoriesSerializer
+
+
+class CategoriesView(APIView):
+
+    def get(self, request):
+        categories = get_list_or_404(Category)
+        serializer = CategoriesSerializer(categories, many=True)
+        return Response(serializer.data, status=200)
